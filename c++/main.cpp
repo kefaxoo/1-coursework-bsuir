060dac8669322d19e3d63ef5db00f11931ea2f8b
Bogdan Petrovskiy <82778510+kefaxoo@users.noreply.github.com>
2022-03-22 20:06:48 +0300

    Update main.cpp

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,143 +1,122 @@
 #include <iostream>
 #include <fstream>
+#include "Wstring.h"
+#include "crypto.h"
+#include <string>
 
 using namespace std;
 
-struct employee {
-    string name;
-    string surname;
-    string middleName;
-    int id;
-    int year; // 0 <= year <= 2022
-    int month; // 1 <= month <= 12
-    int countOfHours;
-    int rate;
+struct user {
+	string login;
+	string hashPassword;
+	bool role; // 1 - admin, 0 - user
+	bool access; // 1 - admin allows access to the system 0 - admin doesn't allow access to the system
 };
 
-int sizeOfName, countOfEmployees;
-char *fileName;
-employee *Employees;
+user *Users;
+int countOfUsers;
 
-string input () {
-    string line;
-    while (getline(cin, line))
-        return line != "" ? line : input();
+struct employee {
+	string name;
+	string surname;
+	string middleName;
+	int ID;
+	int year;
+	int month;
+	int countOfHours;
+	double rate;
+};
+
+user *addUser () {
+	user *newArray = new user[countOfUsers];
+	for (int i = 0; i < countOfUsers - 1; i++)
+		newArray[i] = Users[i];
+
+	swap(newArray, Users);
+	delete [] newArray;
+	return Users;
 }
 
-void cleanMemory () {
-    countOfEmployees = 0;
-    employee *temp = Employees;
-    Employees = new employee[countOfEmployees];
-    delete [] temp;
+void openUserFile () {
+	ifstream openFile("users.txt");
+	if (!openFile.is_open()) {
+		cout << "Файла с пользователями не существует" << endl;
+		exit(0);
+	}
+
+	char line[500];
+	Users = new user[countOfUsers];
+	while (!openFile.eof()) {
+		openFile.getline(line, 500);
+		int count = 0;
+		countOfUsers++;
+		Users = addUser();
+		string temp;
+		for (int i = 0; i < 500 || line[i] != '\0'; i++) {
+			if (line[i] == '|' || line[i] == ';') {
+				switch (count++) {
+					case 0:
+						Users[countOfUsers - 1].login = temp;
+						break;
+					case 1:
+						Users[countOfUsers - 1].hashPassword = temp;
+						break;
+					case 2:
+						Users[countOfUsers - 1].role = stoi(temp);
+						break;
+					case 3:
+						Users[countOfUsers - 1].access = stoi(temp);
+				}
+
+				temp.clear();
+				continue;
+			}
+
+			temp += line[i];
+		}
+	}
+
+	openFile.close();
 }
 
-bool checkIfDatabaseExist () {
-    ifstream openFile(fileName);
-    if (!openFile.is_open()) {
-        openFile.close();
-        cout << endl << "Файл БД не существует" << endl;
-        return false;
-    }
+void login () {
+	cout << "Введите имя пользователя: ";
+	string login = input(), password;
+	int i;
+	while (true) {
+		cout << endl << "Введите пароль: ";
+		password = input();
+		for (i = 0; i < countOfUsers; i++)
+			if (login == Users[i].login) {
+				if (password == decrypt(Users[i].hashPassword))
+					break;
+				else
+					cout << endl << "Введён неправильный пароль. Попробуйте ещё раз!";
+			}
+	}
 
-    return true;
+	if (i != countOfUsers - 1) {
+		if (Users[i].access)
+			if (Users[i].role)
+				cout << endl << "admin" << endl;
+			else
+				cout << endl << "user" << endl;
+	} else {
+		bool temp;
+		cout << endl << "Чтобы зарегестрироваться, необходимо разрешение администратора. Хотите оставить заявку? (1 - да, 0 - нет): ";
+		cin >> temp;
+		if (temp) {
+			cout << endl << "Какие права хотите получить? (1 - администратор, 0 - пользователь): ";
+			cin >> temp;
+			ofstream openFile("users.txt", ios::app);
+			openFile << endl << login << "|" << crypt(password) << "|" << temp << "|" << 0 << ";";
+			openFile.close();
+		}
+	}
 }
 
-void enterDatabaseName () {
-    if (countOfEmployees > 0)
-        cleanMemory();
-
-    cout << endl << "Введите название файла: ";
-    string tempStr = input() + ".txt";
-    sizeOfName = tempStr.length();
-    fileName = new char[sizeOfName];
-    strcpy(fileName, tempStr.c_str());
-    if (!checkIfDatabaseExist())
-        enterDatabaseName();
-}
-
-employee *addEmployee () {
-    employee *newArray = new employee[countOfEmployees];
-    for (int i = 0; i < countOfEmployees - 1; i++)
-        newArray[i] = Employees[i];
-
-    swap(newArray, Employees);
-    delete [] newArray;
-    return Employees;
-}
-
-void readDatabase () {
-    ifstream openFile(fileName);
-    char line[500];
-    Employees = new employee[countOfEmployees];
-    cout << endl << "Чтение БД в оперативную память" << endl;
-    while (!openFile.eof()) {
-        openFile.getline(line, 500);
-        int count = 0;
-        countOfEmployees++;
-        Employees = addEmployee();
-        string temp;
-        for (int i = 0; i < 500 || line[i] != '\0'; i++) {
-            if (line[i] == '|' || line[i] == ';') {
-                switch (count++) {
-                    case 0:
-                        Employees[countOfEmployees - 1].name = temp;
-                        break;
-                    case 1:
-                        Employees[countOfEmployees - 1].surname = temp;
-                        break;
-                    case 2:
-                        Employees[countOfEmployees - 1].middleName = temp;
-                        break;
-                    case 3:
-                        Employees[countOfEmployees - 1].id = stoi(id);
-                        break;
-                    case 4:
-                        Employees[countOfEmployees - 1].year = stoi(temp);
-                        break;
-                    case 5:
-                        Employees[countOfEmployees - 1].month = stoi(temp);
-                        break;
-                    case 6:
-                        Employees[countOfEmployees - 1].countOfHours = stoi(temp);
-                        break;
-                    case 7:
-                        Employees[countOfEmployees - 1].rate = stoi(temp);
-                }
-
-                temp.clear();
-                continue;
-            }
-
-            temp += line[i];
-        }
-    }
-
-    openFile.close();
-    cout << endl << "БД успешно открыта в памяти компьютера" << endl;
-}
-
-void openDatabase () {
-    enterDatabaseName();
-    readDatabase();
-}
-
-int main() {
-    openDatabase();
-
-    while (true) {
-        int menu;
-        cout << endl << "Меню работы с БД \"" << fileName << "\"" << endl;
-        cout 
-        cout << "1 - Режим редактирования" << endl;
-        cout << "2 - Режим обработки данных" << endl;
-        cout << "0 - Завершение работы" << endl;
-        cout << "Ваш выбор: ";
-        cin >> menu;
-        switch (menu) {
-            case 1:
-                editMode();
-                break;
-            case 2:
-        }
-    }
+int main () {
+	setlocale(LC_CTYPE, "RUSSIAN");
+	openUserFile();
+	login();
 }
